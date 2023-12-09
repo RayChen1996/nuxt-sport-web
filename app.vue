87a350bcb85e93e5c8a1bea7a6814a3861039251
RayChen <53119155+RayChen1996@users.noreply.github.com>
2023-12-09 19:42:38 +0800

    init prj

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>11</h1>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+  <div class="p-4">
+    <button class="btn">Hello daisyUI</button>
+  </div>
+  </div>
+</template>
+
+<script></script>
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./assets/css/main.css",
+    "./app.vue",
+    "./src/components/**/*.{js,vue,ts}",
+    "./src/layouts/**/*.vue",
+    "./src/pages/**/*.vue",
+    "./src/plugins/**/*.{js,ts}",
+    "./src/app.vue",
+    "./src/error.vue",
+  ],
+  css: ["/assets/css/main.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+};
